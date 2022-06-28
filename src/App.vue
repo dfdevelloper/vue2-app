1d4c5589c5081f8744b3a61c1ce34b57025c4791
Danillo Fidel <danillopkt@gmail.com>
2022-06-28 17:09:23 -0300

    update

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,60 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3>List of Episodes</h3>
+    <!-- {{ "p" + pending }} -->
+    <div style="display: flex; border: 1px solid red">
+      <div
+        class="shows"
+        v-for="episode in episodes.slice(0, 3)"
+        :key="episode.id"
+      >
+        <div style="display: flex; flex-direction: column; height: 200px">
+          <span>{{ episode.name + " SERVER" }}</span>
+          <img :src="episode.image.medium" />
+        </div>
+      </div>
+
+      <div
+        class="shows"
+        v-for="episode in episodes.slice(3, 6)"
+        :key="episode.id"
+        style="display: flex; border: 1px solid blue"
+      >
+        <div style="display: flex; flex-direction: column; height: 200px">
+          <span>{{ episode.name + "from client" }}</span>
+          <img :src="episode.image.medium" />
+        </div>
+      </div>
+    </div>
+
+    <video
+      v-for="n in 20"
+      :key="n"
+      width="320"
+      height="240"
+      autoplay
+      loop
+      preload="metadata"
+      muted
+    >
+      <source src="@/assets/sample-mp4-file.mp4" type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data: () => ({
+    episodes: [],
+  }),
+  async created() {
+    const d = await fetch("https://api.tvmaze.com/shows/662?embed=episodes");
+    const x = await d.json();
+    this.episodes = x._embedded.episodes;
+  },
+};
 </script>
 
 <style>
